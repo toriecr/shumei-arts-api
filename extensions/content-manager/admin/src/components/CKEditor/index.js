b17c2158be6f29beb662af0ca91188a881d65dc2
toriecr <toriecr@gmail.com>
2021-01-21 17:58:11 -0800

    Resolve missing module

--- extensions/content-manager/admin/src/components/CKEditor/index.js
+++ extensions/content-manager/admin/src/components/CKEditor/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 // import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import CustomEditor from 'ckeditor5-custom-build/build/ckeditor';
+import CustomEditor from 'ckeditor5-custom-build';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
