730f2119170facd6bba2853a6954874b60e0b5ba
toriecr <torie.cr@gmail.com>
2021-03-27 20:08:42 -0700

    Change config

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8080),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '7b3ff1f3a8880314f2ba6595636e899d'),
